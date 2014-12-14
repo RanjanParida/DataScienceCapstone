7572f9a11474df94942f7c4928f28b801230fa3d
Ranjan <priyaranjan.parida@gmail.com>
2014-12-14 15:43:18 -0500

    final version for evaluation

--- loadData.R
+++ loadData.R
@@ -1,18 +1,18 @@
 ## This script is to load the Global Dataset
-blogs <- read.table("data/final.blogs.txt");
+blogs <- read.table(unz("mydata.zip", "data//final.blogs.txt"));
 blogs <- transform(blogs, 
                   V1 = as.character(V1), 
                   V2 = as.character(V2),
                   V3 = as.character(V3));
 
-twitter <- read.table("data/final.twitter.txt");
+twitter <- read.table(unz("mydata.zip", "data//final.twitter.txt"));
 twitter <- transform(twitter, 
                    V1 = as.character(V1), 
                    V2 = as.character(V2),
                    V3 = as.character(V3));
 
 
-news <- read.table("data/final.news.txt");
+news <- read.table(unz("mydata.zip", "data//final.news.txt"));
 news <- transform(news, 
                   V1 = as.character(V1), 
                   V2 = as.character(V2),
